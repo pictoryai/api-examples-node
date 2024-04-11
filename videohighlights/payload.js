adb3554dbe2c732953ea46ca0fcf7fbf68aefe09
Bhaskar <bhaskar@pictory.ai>
2024-04-11 15:09:32 +0530

    Added highlights example

---
+++ videohighlights/payload.js
@@ -0,0 +1,61 @@
+// This function creates payload for auth request
+function createAuthTokenPayload(clientId, clientSecret) {
+    const payload = {
+      client_id: clientId,
+      client_secret: clientSecret
+    };
+    return payload;
+  }
+  
+  // This function creates genrate url payload
+  function createGenerateUrlPayload(filename) {
+    const payload = {
+      contentType: 'video/mp4',
+      fileName: filename
+    };
+    return payload;
+  }
+  
+  // This function creates transcription payload
+  function createTranscriptionPayload(fileUrl, language, webhook) {
+    const payload = {
+      fileUrl: fileUrl,
+      mediaType: "video",
+      language: language,
+      webhook: webhook
+    };
+    return payload;
+  }
+  
+  // This function sets headers
+  function setHeaders(token, userId) {
+    const headers = {
+      Authorization: token,
+      'X-Pictory-User-Id': userId,
+      'Content-Type': 'application/json'
+    };
+    return headers;
+  }
+  
+  // This function sets headers for auth request
+  function setAuthHeaders() {
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+    return headers;
+  }
+  
+  // This function creates highlights payload
+  function createHighlightsPayload(transcript, highlightDuration, webhookUrl) {
+    const payload = {
+      transcript: transcript,
+      highlight_duration: highlightDuration,
+      webhook: webhookUrl
+    };
+    return payload;
+  }
+
+module.exports={createAuthTokenPayload,createGenerateUrlPayload,createTranscriptionPayload,setHeaders,setAuthHeaders,createHighlightsPayload}
+  
+  
+  
